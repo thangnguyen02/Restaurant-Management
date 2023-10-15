515fdc3f4e72ac10ac9323663636d865b50db014
tudda <tentufancr7@gmail.com>
2023-10-15 16:15:57 +0700

    base food and foodcategory

--- app/src/main/java/com/example/restaurantmanagement/Actitvity/FoodActivity.java
+++ app/src/main/java/com/example/restaurantmanagement/Actitvity/FoodActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.restaurantmanagement.R;
 
-public class FoodCategory extends AppCompatActivity {
+public class FoodActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food_category);
+        setContentView(R.layout.activity_food);
     }
 }
--- app/src/main/java/com/example/restaurantmanagement/Actitvity/FoodCategoryActivity.java
+++ app/src/main/java/com/example/restaurantmanagement/Actitvity/FoodCategoryActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.restaurantmanagement.R;
 
-public class Food extends AppCompatActivity {
+public class FoodCategoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food);
+        setContentView(R.layout.activity_food_category);
     }
 }

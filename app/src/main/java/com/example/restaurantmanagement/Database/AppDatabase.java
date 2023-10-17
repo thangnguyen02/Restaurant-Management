c9f17ac1199c56a23d426934cdcbd19a74c98f10
thangdeptrai <nhoctrumgenz2002@gmail.com>
2023-10-17 17:09:03 +0700

    Connect DB

--- app/src/main/java/com/example/restaurantmanagement/Database/AppDatabase.java
+++ app/src/main/java/com/example/restaurantmanagement/Database/AppDatabase.java
@@ -1,14 +1,31 @@
 package com.example.restaurantmanagement.Database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import com.example.restaurantmanagement.Models.ComboTable;
 import com.example.restaurantmanagement.Models.FoodTable;
 import com.example.restaurantmanagement.Models.TableOrder;
 import com.example.restaurantmanagement.Models.TableOrderCustomer;
 
+import org.w3c.dom.CDATASection;
+
 @Database(entities = {TableOrder.class, TableOrderCustomer.class, FoodTable.class, ComboTable.class},version = 1)
-public abstract class AppDatabase {
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase database;
+    private static String DATABASE_NAME;
+
+    public synchronized static AppDatabase getInstance(Context context) {
+        if (database == null) {
+            AppDatabase database = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, DATABASE_NAME).allowMainThreadQueries().fallbackToDestructiveMigration().build();
+
+        }
+        return database;
+    }
     public abstract TableOrderDAO tableOrderDAO();
     public abstract  TableOrderCustomerDAO tableOrderCustomerDAO();
     public abstract  FoodTableDAO foodTableDAO();
--- app/src/main/java/com/example/restaurantmanagement/MainActivity.java
+++ app/src/main/java/com/example/restaurantmanagement/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.restaurantmanagement;
 
         import androidx.appcompat.app.AppCompatActivity;
+        import androidx.room.Room;
 
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+        import com.example.restaurantmanagement.Database.AppDatabase;
+        import com.example.restaurantmanagement.Database.TableOrderDAO;
+        import com.example.restaurantmanagement.Models.TableOrder;
+
+        import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    AppDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //connect db
+        database = AppDatabase.getInstance(this);
         setContentView(R.layout.activity_main);
+
     }
+
 }

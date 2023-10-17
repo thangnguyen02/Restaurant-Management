c19c4a166119c9beed4ac9a2f6b97940d291c284
tudda <tentufancr7@gmail.com>
2023-10-17 22:22:05 +0700

    .

--- app/src/main/java/com/example/restaurantmanagement/MainActivity.java
+++ app/src/main/java/com/example/restaurantmanagement/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.restaurantmanagement;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-        import android.os.Bundle;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.restaurantmanagement.Adapters.FoodCategoryAdapter;
+import com.example.restaurantmanagement.Models.FoodCategory;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 

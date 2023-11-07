f9bca89d0d472603266330ec348c5ca3af7e114b
nhat <nhatyhnl1258@gmail.com>
2023-11-07 08:48:56 +0700

    fix order

--- app/src/main/java/com/example/restaurantmanagement/Activity/ListTableActivity.java
+++ app/src/main/java/com/example/restaurantmanagement/Activity/ListTableActivity.java
@@ -1,13 +1,14 @@
 package com.example.restaurantmanagement.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
 
 import com.example.restaurantmanagement.Adapters.TableListsAdapter;
 import com.example.restaurantmanagement.Database.AppDatabase;
@@ -20,22 +21,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListTableActivity extends AppCompatActivity {
-    AppDatabase database;
-    ImageView back;
-    List<TableOrder> tables = new ArrayList<>();
-    RecyclerView recyclerView;
-    TableListsAdapter tableListAdapter;
-    FloatingActionButton f;
+    private AppDatabase database;
+    private ImageView back;
+    private List<TableOrder> tables = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private TableListsAdapter tableListAdapter;
+    private FloatingActionButton f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_table);
+
         database = AppDatabase.getInstance(this);
-         tables = database.tableOrderDAO().getAll();
         recyclerView = findViewById(R.id.recycler_table);
         back = findViewById(R.id.back);
         f = findViewById(R.id.f_add);
-        updateRecycler(tables);
+
+        loadTables(); // Load tables initially
+
         f.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +47,7 @@ public class ListTableActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,19 +55,42 @@ public class ListTableActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 
+    // Load tables asynchronously
+    private void loadTables() {
+        new LoadTablesTask().execute();
+    }
+
+    // AsyncTask to load tables in the background
+    private class LoadTablesTask extends AsyncTask<Void, Void, List<TableOrder>> {
+        @Override
+        protected List<TableOrder> doInBackground(Void... voids) {
+            return database.tableOrderDAO().getAll();
+        }
+
+        @Override
+        protected void onPostExecute(List<TableOrder> result) {
+            super.onPostExecute(result);
+            updateRecycler(result);
+        }
+    }
+
+    // Update the RecyclerView with the new data
     private void updateRecycler(List<TableOrder> tables) {
+        this.tables = tables;
         recyclerView.setHasFixedSize(true);
-        tableListAdapter = new TableListsAdapter(ListTableActivity.this,tables,tableOrderClickListener);
+        tableListAdapter = new TableListsAdapter(ListTableActivity.this, tables, tableOrderClickListener);
         recyclerView.setAdapter(tableListAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 
     private final TableOrderClickListener tableOrderClickListener = new TableOrderClickListener() {
         @Override
         public void onClick(TableOrder tableOrder) {
-
+            // Handle table click event if needed
         }
     };
 }
